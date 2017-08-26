097c9683fab881325d38a6b5644f9919ea117af9
Joachim Metz <joachim.metz@gmail.com>
2017-08-26 08:52:16 +0200

    Worked on tests

---
+++ tests/test_fsrefsinfo.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Info tool testing script
+#
+# Version: 20170825
+
+EXIT_SUCCESS=0;
+EXIT_FAILURE=1;
+EXIT_IGNORE=77;
+
+OPTION_SETS="";
+OPTIONS="";
+
+INPUT_GLOB="*";
+
+if ! test -z ${SKIP_TOOLS_TESTS};
+then
+	exit ${EXIT_IGNORE};
+fi
+
+TEST_EXECUTABLE="../fsrefstools/fsrefsinfo";
+
+if ! test -x "${TEST_EXECUTABLE}";
+then
+	TEST_EXECUTABLE="../fsrefstools/fsrefsinfo.exe";
+fi
+
+if ! test -x "${TEST_EXECUTABLE}";
+then
+	echo "Missing test executable: ${TEST_EXECUTABLE}";
+
+	exit ${EXIT_FAILURE};
+fi
+
+TEST_RUNNER="tests/test_runner.sh";
+
+if ! test -f "${TEST_RUNNER}";
+then
+	TEST_RUNNER="./test_runner.sh";
+fi
+
+if ! test -f "${TEST_RUNNER}";
+then
+	echo "Missing test runner: ${TEST_RUNNER}";
+
+	exit ${EXIT_FAILURE};
+fi
+
+source ${TEST_RUNNER};
+
+run_test_on_input_directory "fsrefsinfo" "fsrefsinfo" "with_stdout_reference" "${OPTION_SETS}" "${TEST_EXECUTABLE}" "input" "${INPUT_GLOB}" "${OPTIONS}";
+RESULT=$?;
+
+exit ${RESULT};
+
